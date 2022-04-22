a04282c193be317ca2f3007fb9de7af663348530
deepizel <rocmancave@gmail.com>
2022-04-22 14:56:24 +0100

    updated js

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
-const button = document.querySelector("button");
-const popup = document.querySelector(".nxtcard");
-const close = document.querySelector(".closx");
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const btn3 = document.querySelector(".btn3");
-const btn4 = document.querySelector(".btn4");
-const popupTxt = document.querySelector(".reviews");
-const more = document.querySelector(".more");
+// const button = document.querySelector("button");
+// const popup = document.querySelector(".nxtcard");
+// const close = document.querySelector(".closx");
+// const btn1 = document.querySelector(".btn1");
+// const btn2 = document.querySelector(".btn2");
+// const btn3 = document.querySelector(".btn3");
+// const btn4 = document.querySelector(".btn4");
+// const popupTxt = document.querySelector(".reviews");
+// const more = document.querySelector(".more");
 
-btn1.addEventListener("click", () => {
-  btn2.style.display = "none";
-  btn3.style.display = "none";
-  btn4.style.display = "none";
-  popupTxt.innerText = "Great team.. Loved their work, will definatly use them again";
-  more.style.display = "none";
+// btn1.addEventListener("click", () => {
+//   btn2.style.display = "none";
+//   btn3.style.display = "none";
+//   btn4.style.display = "none";
+//   popupTxt.innerText = "Great team.. Loved their work, will definatly use them again";
+//   more.style.display = "none";
 
-});
+// });
 
-btn2.addEventListener("click", () => {
-  btn1.style.display = "none";
-  btn3.style.display = "none";
-  btn4.style.display = "none";
-  popupTxt.innerText = "Funcity is amazing and we woudnt have loved our decour either way ";
-  more.style.display = "none";
-});
+// btn2.addEventListener("click", () => {
+//   btn1.style.display = "none";
+//   btn3.style.display = "none";
+//   btn4.style.display = "none";
+//   popupTxt.innerText = "Funcity is amazing and we woudnt have loved our decour either way ";
+//   more.style.display = "none";
+// });
 
-btn3.addEventListener("click", () => {
-  btn1.style.display = "none";
-  btn2.style.display = "none";
-  btn4.style.display = "none";
-  popupTxt.innerText = "I was simply stunned to watch my room transform, they did a splendid job";
-  more.style.display = "none";
-});
+// btn3.addEventListener("click", () => {
+//   btn1.style.display = "none";
+//   btn2.style.display = "none";
+//   btn4.style.display = "none";
+//   popupTxt.innerText = "I was simply stunned to watch my room transform, they did a splendid job";
+//   more.style.display = "none";
+// });
 
-btn4.addEventListener("click", () => {
-  btn1.style.display = "none";
-  btn2.style.display = "none";
-  btn3.style.display = "none";
-  popupTxt.innerText = "These guys are really dope and i already referred them to several people";
-  more.style.display = "none";
-});
+// btn4.addEventListener("click", () => {
+//   btn1.style.display = "none";
+//   btn2.style.display = "none";
+//   btn3.style.display = "none";
+//   popupTxt.innerText = "These guys are really dope and i already referred them to several people";
+//   more.style.display = "none";
+// });
+
+const images = document.querySelectorAll("#reviewz");
+
+let i = 0;
+setInterval(function () {
+  if (i == 0) {
+    images[i].style.display = "block";
+  } else if (i == images.length) {
+    images[i - 1].style.display = "none";
+    images[0].style.display = "block";
+    i = 0;
+  } else {
+    images[i - 1].style.display = "none";
+    images[i].style.display = "block";
+  }
+
+  i++;
+}, 2000);
